22cb7215641ed1a8bc883028c795bb852dc3732a
Mapacherama <48964493+Mapacherama@users.noreply.github.com>
2022-07-18 08:35:32 +0200

    Added the speaker API.

--- resolvers.js
+++ resolvers.js
@@ -6,6 +6,12 @@ module.exports = {
       sessionById: (parent, { id }, { dataSources }, info) => {
         return dataSources.sessionAPI.getSessionById(id);
       },
+      speakers: (parent, args, { dataSources }, info) => {
+        return dataSources.speakerAPI.getSpeakers();
+      },
+      speakerById: (parent, { id }, { dataSources }, info) => {
+        return dataSources.speakerAPI.getSpeakerById(id);
+      },
     },
   };
   
