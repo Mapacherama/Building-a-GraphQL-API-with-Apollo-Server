5df8ef7f5b6b4e3b5822deea48a0e7676f313a1c
JeromeTesselaar <jtesselaar@snelstart.nl>
2022-07-19 12:37:09 +0200

    Module 4 is finished.

--- resolvers.js
+++ resolvers.js
@@ -1,28 +1,8 @@
-const _ = require('lodash');
-module.exports = {
-    Query: {
-      sessions: (parent, args, { dataSources }, info) => {
-        return dataSources.sessionAPI.getSessions(args);
-      },
-      sessionById: (parent, { id }, { dataSources }, info) => {
-        return dataSources.sessionAPI.getSessionById(id);
-      },
-      speakers: (parent, args, { dataSources }, info) => {
-        return dataSources.speakerAPI.getSpeakers();
-      },
-      speakerById: (parent, { id }, { dataSources }, info) => {
-        return dataSources.speakerAPI.getSpeakerById(id);
-      },
-    },
-    Session: {
-        async speakers(session,args, {dataSources}){
-            const speakers = await dataSources.speakerAPI.getSpeakers();
-            const returns = speakers.filter((speaker)=> {
-                return _.filter(session.speakers, {id: speaker.id}).length > 0
-            });
 
-            return returns;
-        }
-    }
+const Query = require('./resolvers/query')
+const Session = require('./resolvers/session')
+module.exports = {
+    Query,
+    Session 
   };
   
